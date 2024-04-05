a95a8087b0cdc9c8e6c02ecaf47e755f220827b7
rabbit-313 <migiso1051@i.softbank.jp>
2024-04-05 13:49:49 +0900

    add fit

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -24,7 +24,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
 
 
 ##############################################################
